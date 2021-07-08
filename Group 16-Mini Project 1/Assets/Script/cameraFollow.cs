72d153f36a165c829f211b212c88c6b75ea80c6a
longcualonglonglong11 <nhlong18@apcs.vn>
2021-07-09 01:21:04 +0700

    add lv1

---
+++ Group 16-Mini Project 1/Assets/Script/cameraFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    Transform queenTransform;
+    float leftBarrier = -4;
+    float rightBarrier = 10.0f;
+    void Start()
+    {
+        queenTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 curPosition = transform.position;
+        curPosition.x = leftBarrier;
+        curPosition.y = 3f;
+        transform.position = curPosition;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 curPosition = transform.position;
+/*        if (queenTransform.position.x > leftBarrier && queenTransform.position.x < rightBarrier){
+*/            curPosition.x = queenTransform.position.x;
+     /*   } */
+        curPosition.y = queenTransform.position.y;
+        transform.position = curPosition;
+    }
+}
